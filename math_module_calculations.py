d53275897037bf374d3d29bd5abbd9fe3d2412ca
namanbagrecha <namanjain007.0@gmail.com>
2025-07-26 17:42:35 +0530

    Create math_module_calculations.py

---
+++ math_module_calculations.py
@@ -0,0 +1,8 @@
+from colorama import Fore
+import math
+
+num = float(input(f"{Fore.MAGENTA}Enter a number: "))
+print(f"{Fore.MAGENTA}Square root: {math.sqrt(num)}")
+print(f"{Fore.MAGENTA}Logarithm: {math.log(num)}")
+print(f"{Fore.MAGENTA}Sine: {math.sin(num)}")
+
